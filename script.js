cb5ade6bf126de453cf2bf5c16a9f5b12a80f2be
Shubhanshu Mishra <shubhmishra1749@gmail.com>
2026-03-19 02:31:10 +0530

    added dropdown feature

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ document.createElement("smth")
     -> this command is used to create that element in browser's memory (dynamically)
 document.getElementById("smth")
     smth -> some unique name from html 
-*/
 
+addEventListener() : is a built-in method used to attach an event handler (a function) to a specific element in the Document Object Model (DOM). 
+                   : It allows a web page to "listen" for user interactions—such as clicks, key presses, or mouse movements—and execute code in response.
+*/
+let contextMenu = document.createElement("div");
+contextMenu.className = "context-menu"; 
+document.body.appendChild(contextMenu); // why this is needed anyway ?? 
+    // you created this element in memory but it won't show on screen unless you add it to DOM
 let currentFilter = "all"; 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 /*
@@ -15,6 +21,23 @@ returns string if data exists or null o/w.
 
 parse => converts string -> to js object / array 
 */
+
+document.addEventListener("click", function(e) {
+    // Listens to every click on the page
+    // parameters - event, function // another eg .onclick 
+    if(!contextMenu.contains(e.target)) {
+        contextMenu.style.display = "none";
+    }
+});
+document.getElementById("taskInput").addEventListener("keypress", function(e) {
+    // why not just document.addEventListener() as it is done above?
+        // You can, but:
+            // document.addEventListener() → listens to ALL keypresses on page
+            // getElementById("taskInput") → listens only inside input box, so it works only when the text cursor is in the input box 
+    if(e.key == "Enter") {
+        addTask();
+    }
+});
 
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -36,7 +59,6 @@ function renderTasks() {
     list.innerHTML = ""; 
     /* removes all existing tasks from screen 
         we don't update UI manually , we rebuild it */
-
     tasks.forEach((task , index) => {
     /*
     loop over each task , task = current task object 
@@ -61,109 +83,14 @@ function renderTasks() {
         li.onclick = function() {
             tasks[index].done = !tasks[index].done; 
             saveTasks(); 
-            renderTasks();  
+            renderTasks(); 
         }; 
+
+        li.oncontextmenu = function(e) { // function called on the right click
+            e.preventDefault(); // stops the browswer's default menu
+            showMenu(e, index); 
+        }
         
-        let menuBtn = document.createElement("button");
-        menuBtn.innerText = "⋮";
-        menuBtn.className = "menu-btn";
-
-        let menu = document.createElement("div"); // creates a HTML container in browser's memory 
-        menu.style.display = "none"; // initially the element will be hidden
-        
-        let editBtn = document.createElement("button"); 
-        editBtn.innerText = "Edit";
-        editBtn.className = "edit-btn";
-        editBtn.onclick = function(e) {
-            e.stopPropagation(); // shouldn't affect the parent i.e. <li>
-            
-            // 1st way - uses prompt , then write the newText in the new window itself
-            let newText = prompt("Edit Task:", task.text);
-
-            if(newText !== null && newText.trim() !== "") {
-                tasks[index].text = newText.trim(); 
-                saveTasks(); 
-                renderTasks(); 
-            }
-        };
-        let deleteBtn = document.createElement("button"); 
-        deleteBtn.className = "delete-btn";
-        deleteBtn.innerText = "Delete"; 
-        // what is e here ? e = event object , e can be click, keypress , mousemove.
-        // here it is button-click
-        deleteBtn.onclick = function(e) {
-            // runs when delete button is clicked
-            e.stopPropagation(); // o/w clicking delete also triggers li.onclick 
-            tasks.splice(index, 1); // removes 1 element at position index 
-            saveTasks(); 
-            renderTasks(); 
-        };
-        // toggle menu 
-        menuBtn.onclick = function(e) {
-            e.stopPropagation();
-            menu.style.display = (menu.style.display === "none") ? "block" : "none"; 
-            // this is a toggle , if menu is hidden => show it, and hide it o/w.
-        }
-
-        /* --> this was for explicity adding delete and edit buttons on each task 
-
-
-        // buttons : delete-btn and edit-btn 
-        let deleteBtn = document.createElement("button"); 
-        deleteBtn.className = "delete-btn";
-        deleteBtn.innerText = "Delete"; 
-        // what is e here ? e = event object , e can be click, keypress , mousemove.
-        // here it is button-click
-        deleteBtn.onclick = function(e) {
-            // runs when delete button is clicked
-            e.stopPropagation(); // o/w clicking delete also triggers li.onclick 
-            tasks.splice(index, 1); // removes 1 element at position index 
-            saveTasks(); 
-            renderTasks(); 
-        }
-        let editBtn = document.createElement("button"); 
-        editBtn.innerText = "Edit";
-        editBtn.onclick = function(e) {
-            e.stopPropagation(); // shouldn't affect the parent i.e. <li>
-            
-            // 1st way - uses prompt , then write the newText in the new window itself
-            let newText = prompt("Edit Task:", task.text);
-
-            if(newText !== null && newText.trim() !== "") {
-                tasks[index].text = newText.trim(); 
-                saveTasks(); 
-                renderTasks(); 
-            }
-            
-            // 2nd way - create input element
-
-//            // li.innerHTML = ""; // what does this do exactly ? like what is the content it captures ? does this include the buttons as well?
-            // let input = document.createElement("input"); 
-            // input.value = task.text; // why this update ? won't it change the content of input.value
-
-            // li.firstChild.nodeValue = ""; 
-
-            // input.focus(); // puts cursor inside the input box
-            // input.onkeypress = function(e) {
-            //     if(e.key == "Enter") {
-            //         if(input.value.trim() !== "")
-            //         tasks[index].text = input.value.trim();
-            //         saveTasks();
-            //         renderTasks(); 
-            //     }
-            // }
-
-            // input.onblur = function() {
-            //     renderTasks(); 
-            // }
-        }
-        */
-        li.appendChild(menuBtn);
-        li.appendChild(menu); 
-
-        menu.appendChild(deleteBtn); 
-        menu.appendChild(editBtn); // o/w this edit button won't appear
-        /* put button inside <li> */
         list.appendChild(li); 
         /* add <li> to <ul> */
     });
@@ -174,9 +101,50 @@ function filter() {
     renderTasks(); 
 }
 
-// function editTasks() {
-//     let input = document.createElement("input");
-// }
+function showMenu(e, index) {
+    contextMenu.innerHTML = ""; 
+
+    // edit button 
+    let editBtn = document.createElement("button"); 
+    editBtn.innerText = "Edit";
+    editBtn.className = "edit-btn";
+    editBtn.onclick = function(e) {
+        // e.stopPropagation(); // shouldn't affect the parent i.e. <li>
+        
+        // 1st way - uses prompt , then write the newText in the new window itself
+        let newText = prompt("Edit Task:", tasks[index].text);
+
+        if(newText !== null && newText.trim() !== "") {
+            tasks[index].text = newText.trim(); 
+            saveTasks(); 
+            renderTasks(); 
+        }
+        contextMenu.style.display = "none";
+    };
+    // delete button 
+    let deleteBtn = document.createElement("button"); 
+    deleteBtn.className = "delete-btn";
+    deleteBtn.innerText = "Delete"; 
+    // what is e here ? e = event object , e can be click, keypress , mousemove.
+    // here it is button-click
+    deleteBtn.onclick = function(e) {
+        // runs when delete button is clicked
+        // e.stopPropagation(); // o/w clicking delete also triggers li.onclick 
+        tasks.splice(index, 1); // removes 1 element at position index 
+        saveTasks(); 
+        renderTasks(); 
+        contextMenu.style.display = "none";
+    };
+
+    contextMenu.appendChild(editBtn); 
+    contextMenu.appendChild(deleteBtn);
+
+    contextMenu.style.left = e.pageX + "px"; // appending a px unit only 
+    contextMenu.style.top = e.pageY + "px";
+
+    contextMenu.style.display = "block";
+}
+
 function addTask() {
     let input = document.getElementById("taskInput");
 

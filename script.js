cfc0153e648d6b04091942b217b7a99b222d34d9
Shubhanshu Mishra <shubhmishra1749@gmail.com>
2026-03-19 01:05:25 +0530

    added buttons - edit , delete into a single menu button, colored the task based on their states, using classList.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+document.createElement("smth") 
+    smth -> div [ creates a new HTML container], button , option , li etc 
+    -> this command is used to create that element in browser's memory (dynamically)
+document.getElementById("smth")
+    smth -> some unique name from html 
+*/
+
 let currentFilter = "all"; 
 let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 /*
@@ -38,6 +46,13 @@ function renderTasks() {
             return;  
         }
         let li = document.createElement("li"); 
+
+        // It adds a CSS class (either done or pending) to each <li> based on its state
+        if(task.done) {
+            li.classList.add("done"); 
+        } else {
+            li.classList.add("pending");
+        }
         /* creates <li> element */
         li.innerText = task.text; /* sets text inside <li> */
         if(task.done) {
@@ -49,17 +64,105 @@ function renderTasks() {
             renderTasks();  
         }; 
         
+        let menuBtn = document.createElement("button");
+        menuBtn.innerText = "⋮";
+        menuBtn.className = "menu-btn";
+
+        let menu = document.createElement("div"); // creates a HTML container in browser's memory 
+        menu.style.display = "none"; // initially the element will be hidden
+        
+        let editBtn = document.createElement("button"); 
+        editBtn.innerText = "Edit";
+        editBtn.className = "edit-btn";
+        editBtn.onclick = function(e) {
+            e.stopPropagation(); // shouldn't affect the parent i.e. <li>
+            
+            // 1st way - uses prompt , then write the newText in the new window itself
+            let newText = prompt("Edit Task:", task.text);
+
+            if(newText !== null && newText.trim() !== "") {
+                tasks[index].text = newText.trim(); 
+                saveTasks(); 
+                renderTasks(); 
+            }
+        };
         let deleteBtn = document.createElement("button"); 
+        deleteBtn.className = "delete-btn";
         deleteBtn.innerText = "Delete"; 
+        // what is e here ? e = event object , e can be click, keypress , mousemove.
+        // here it is button-click
         deleteBtn.onclick = function(e) {
-            /* runs when delete button is clicked */
-            e.stopPropagation(); /* o/w clicking delete also triggers li.onclick */
-            tasks.splice(index, 1); /* removes 1 element at position index */
+            // runs when delete button is clicked
+            e.stopPropagation(); // o/w clicking delete also triggers li.onclick 
+            tasks.splice(index, 1); // removes 1 element at position index 
             saveTasks(); 
             renderTasks(); 
         };
+        // toggle menu 
+        menuBtn.onclick = function(e) {
+            e.stopPropagation();
+            menu.style.display = (menu.style.display === "none") ? "block" : "none"; 
+            // this is a toggle , if menu is hidden => show it, and hide it o/w.
+        }
 
-        li.appendChild(deleteBtn); 
+        /* --> this was for explicity adding delete and edit buttons on each task 
+
+
+        // buttons : delete-btn and edit-btn 
+        let deleteBtn = document.createElement("button"); 
+        deleteBtn.className = "delete-btn";
+        deleteBtn.innerText = "Delete"; 
+        // what is e here ? e = event object , e can be click, keypress , mousemove.
+        // here it is button-click
+        deleteBtn.onclick = function(e) {
+            // runs when delete button is clicked
+            e.stopPropagation(); // o/w clicking delete also triggers li.onclick 
+            tasks.splice(index, 1); // removes 1 element at position index 
+            saveTasks(); 
+            renderTasks(); 
+        }
+        let editBtn = document.createElement("button"); 
+        editBtn.innerText = "Edit";
+        editBtn.onclick = function(e) {
+            e.stopPropagation(); // shouldn't affect the parent i.e. <li>
+            
+            // 1st way - uses prompt , then write the newText in the new window itself
+            let newText = prompt("Edit Task:", task.text);
+
+            if(newText !== null && newText.trim() !== "") {
+                tasks[index].text = newText.trim(); 
+                saveTasks(); 
+                renderTasks(); 
+            }
+            
+            // 2nd way - create input element
+
+//            // li.innerHTML = ""; // what does this do exactly ? like what is the content it captures ? does this include the buttons as well?
+            // let input = document.createElement("input"); 
+            // input.value = task.text; // why this update ? won't it change the content of input.value
+
+            // li.firstChild.nodeValue = ""; 
+
+            // input.focus(); // puts cursor inside the input box
+            // input.onkeypress = function(e) {
+            //     if(e.key == "Enter") {
+            //         if(input.value.trim() !== "")
+            //         tasks[index].text = input.value.trim();
+            //         saveTasks();
+            //         renderTasks(); 
+            //     }
+            // }
+
+            // input.onblur = function() {
+            //     renderTasks(); 
+            // }
+        }
+        */
+        li.appendChild(menuBtn);
+        li.appendChild(menu); 
+
+        menu.appendChild(deleteBtn); 
+        menu.appendChild(editBtn); // o/w this edit button won't appear
         /* put button inside <li> */
         list.appendChild(li); 
         /* add <li> to <ul> */
@@ -71,9 +174,19 @@ function filter() {
     renderTasks(); 
 }
 
+// function editTasks() {
+//     let input = document.createElement("input");
+// }
 function addTask() {
     let input = document.getElementById("taskInput");
 
+    /*
+    document.getElementById("taskInput").addEventListener("keypress", function(e) {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    });
+    */
     if(input.value.trim()==="") {
         alert("Tasks is invalid");
         renderTasks();  
@@ -92,7 +205,7 @@ function addTask() {
 }
 
 /* 
-    add edit task
+    add edit task -> done using prompt method
     add filters -> done 
     add deadline 
     add dark Mode 
